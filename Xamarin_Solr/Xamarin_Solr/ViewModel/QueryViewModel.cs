22bb5e88dc0bb89099a10d90288c9b43d6295c94
DO_SK <DO_SK@W10-DO-SK>
2017-04-28 07:59:41 +0200

    first simple DataBinding

--- Xamarin_Solr/Xamarin_Solr/ViewModel/QueryViewModel.cs
+++ Xamarin_Solr/Xamarin_Solr/ViewModel/QueryViewModel.cs
@@ -26,6 +26,15 @@ namespace Xamarin_Solr.ViewModel
             Title = "Customer List";
             Icon = "icon.png";
             IsBusy = true;
+            ListOfDocuments = new ObservableCollection<BigDatDocument>();
+            var docu = new BigDatDocument();
+            docu.Title = "Test-Title";
+            docu.Id = "I-ETTEST";
+            docu.PubDate = 234124515;
+            docu.Content = "langere Content, sollte eventuell in finalen Version auf separater Seite dann angezeigt werden,\n"
+                + " dadurch bleibt alles etwas übersichtlicher und wir hätten eine Art simple Navigation in der App.";
+
+            ListOfDocuments.Add(docu);   
 
         }
     }
